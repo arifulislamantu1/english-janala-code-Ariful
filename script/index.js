3d54e511f7af6140677b88a977503862d071c1cd
arifulislamantu1 <arifulislamantu1@gmail.com>
2026-03-08 05:32:39 +0600

    improve lesson section UX with active buttons and error handling

--- script/index.js
+++ script/index.js
@@ -5,24 +5,44 @@ const loadLessons =()=>{
     .then(json => displayLessons(json.data))
 };
 
+const removeActive = () => {
+    const lessonBtn = document.querySelectorAll('.lesson-btn');
+    lessonBtn.forEach(btn => btn.classList.remove("active"))
+};
+
 const loadLevelWod =(id)=>{
     const url = `https://openapi.programming-hero.com/api/level/${id}`;
     fetch(url)
     .then(res => res.json())
-    .then(data => displayLevelWord(data.data))
+    .then(data => {
+        removeActive(); //remove all active class
+        const clickBtn = document.getElementById(`lesson-btn-${id}`);
+        clickBtn.classList.add("active"); // add active class
+        displayLevelWord(data.data);
+})
 };
 
 const displayLevelWord = (words) =>{
     const wordContainer = document.getElementById('word-container')
     wordContainer.innerHTML = '';
 
+    if(words.length === 0){
+        wordContainer.innerHTML = `<div class="text-center col-span-3  py-10 space-y-6 font-bangla">
+        <img class="mx-auto" src="./assets/alert-error.png" alt="">
+        <p class="text-xl font-medium text-gray-400 ">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+        <h2 class="font-bold text-4xl">নেক্সট Lesson এ যান</h2>
+       </div>`;
+
+        return;
+    }
+
     words.forEach(word =>{
         const card = document.createElement('div');
         card.innerHTML = `
         <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
-        <h2 class=" font-bold text-2xl">${word.word}</h2>
+        <h2 class=" font-bold text-2xl">${word.word? word.word : 'শব্দ পাওয়া যায়নি'}</h2>
         <p class="font-semibold">Meaning / Pronunciation</p>
-        <div class="font-bangla trxt-2xl font-semibold">"${word.meaning} / ${word.pronunciation}"</div>
+        <div class="font-bangla trxt-2xl font-semibold">"${word.meaning? word.meaning : 'অর্থ পাওয়া যায়নি'} / ${word.pronunciation? word.pronunciation : 'Pronunciation পাওয়া যায়নি'}"</div>
         <div class="flex justify-between items-center">
           <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
           <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
@@ -47,7 +67,7 @@ for(let lesson of lessons){
 const btnDiv = document.createElement('div');
 
 btnDiv.innerHTML =`
-            <button onclick="loadLevelWod(${lesson.level_no})" class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i>Lesson -${lesson.level_no} </button>
+            <button id="lesson-btn-${lesson.level_no}" onclick="loadLevelWod(${lesson.level_no})" class="btn btn-outline btn-primary lesson-btn"><i class="fa-solid fa-book-open"></i>Lesson -${lesson.level_no} </button>
 `;
 
 //  4. append  into container
